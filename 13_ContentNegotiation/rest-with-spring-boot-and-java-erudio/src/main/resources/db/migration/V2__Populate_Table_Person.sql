13bd1ad3cc89cf65a80e6200f50760bf74217456
Lucas Eduardo Volkmann <lucas.volkmann2001@gmail.com>
2023-10-29 19:42:04 -0300

    session 13

---
+++ 13_ContentNegotiation/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES 
+(1,'Blumenau - SC','Lucas','Male','Eduardo'),
+(2,'teste\'s address','teste - updated','Gender','2º registro'),
+(6,'Lomas de Zamora - Argentina','Germán','Male','Cano'),
+(7,'teste\'s address','person version 2','Gender','test'),
+(8,'teste\'s address','person version 1','Gender','test');
